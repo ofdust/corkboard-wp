fc4ba901a36f38b649171972d6e5fa47efa3bf73
Shelby <captain.norcross@gmail.com>
2017-04-27 10:24:32 -0600

    portfolio template created and given basic styling

--- portfolio-page.php
+++ portfolio-page.php
@@ -1,21 +1,9 @@
-<?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package corkboard
- */
+<?php /* Template Name: portfolio */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="portfolio-primary" class="content-area">
+		<main id="portfolio-main" class="site-main" role="main">
 
 			<?php
 			while ( have_posts() ) : the_post();
